8edbae9ab4b5e343c5844346f07761c2d06313b0
Peter Karlsson <peter.karlsson90@icloud.com>
2018-08-21 10:28:04 +0200

    first commit

---
+++ Assets/Script/TileController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+    public TileManager tileManager;
+    public bool empty = false;
+
+
+    private void OnMouseDown()
+    {
+        tileManager.TilePressed(this);
+    }
+
+
+
+}
---
+++ Assets/Script/TileManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public List<TileController> tiles;
+
+    public void TilePressed(TileController tile) {
+        Debug.Log("Tile pressed: " + tile.gameObject.name);
+    }
+
+
+	
+}
