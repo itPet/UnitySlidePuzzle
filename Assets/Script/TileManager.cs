80f2eab14ceab5e8df8f3bbd38b393f718bf1fa6
Peter Karlsson <peter.karlsson90@icloud.com>
2018-08-21 11:28:36 +0200

    Can move tiles correctly

--- Assets/Script/TileManager.cs
+++ Assets/Script/TileManager.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour {
 
+    int rowSize;
+
     public List<TileController> tiles;
 
-    public void TilePressed(TileController tile) {
-        Debug.Log("Tile pressed: " + tile.gameObject.name);
-        Debug.Log("Test. Remove this line");
-        //More test stuff
+    private void Start() {
+        rowSize = (int) Mathf.Sqrt(tiles.Count);
     }
 
+    public void TilePressed(TileController tile) {
+        TileController emptyNeighbour = EmptyNeighbour(tile);
+        if (emptyNeighbour != null)
+            SwitchTiles(tile, emptyNeighbour);
+    }
 
+    void SwitchTiles(TileController tile1, TileController tile2) {
+        //Switch fysical objects
+        Vector3 pos = tile1.gameObject.transform.position;
+        tile1.gameObject.transform.position = tile2.gameObject.transform.position;
+        tile2.gameObject.transform.position = pos;
 
+        //Switch in list
+        int index1 = TileIndex(tile1);
+        int index2 = TileIndex(tile2);
+
+        tiles[index1] = tile2;
+        tiles[index2] = tile1;
+    }
+
+    TileController EmptyNeighbour(TileController tile) {
+        foreach( TileController t in Neighbours(tile)) {
+            if (t.empty)
+                return t;
+        }
+
+        return null;
+    }
+
+    List<TileController> Neighbours(TileController tile) {
+        int index = TileIndex(tile);
+        List<TileController> neighbours = new List<TileController>();
+
+        int over = index - rowSize;
+        if(InRange(over))
+            neighbours.Add(tiles[over]);
+
+        int under = index + rowSize;
+        if (InRange(under))
+            neighbours.Add(tiles[under]);
+
+        int right = index + 1;
+        if (InRange(right) && (index + 1) % rowSize != 0)
+            neighbours.Add(tiles[right]);
+
+        int left = index - 1;
+        if (InRange(left) && index % rowSize != 0)
+            neighbours.Add(tiles[left]);
+
+        return neighbours;
+    }
+
+    bool InRange(int index) {
+        return (index >= 0 && index < tiles.Count);
+    }
+
+    int TileIndex(TileController tile) {
+        return tiles.IndexOf(tile);
+    }
 	
 }
